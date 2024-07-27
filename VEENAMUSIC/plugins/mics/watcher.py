15759565c019962565e2c9cfef0fe87fa23a2ce9
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-07-27 22:32:22 +0530

    Add files via upload

---
+++ VEENAMUSIC/plugins/mics/watcher.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from VEENAMUSIC import app
+from VEENAMUSIC.core.call import veena
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await veena.stop_stream_force(message.chat.id)
